5c79786e31067a78987513e912544a9ca32a592e
Gabriel Oliveira <gabriel_oliveira_f_sousa@outlook.com>
2020-04-30 13:24:08 -0300

    Busca por profundidade II

--- src/buscas/profundidade.java
+++ src/buscas/profundidade.java
@@ -6,40 +6,48 @@ import grafocidades.Cidade;
 import grafocidades.Mapa;
 
 public class profundidade {
-	
+
 	private Pilha fronteira;
 	private Cidade inicio;
 	private Cidade objetivo;
-	
-	public profundidade(Cidade inicio,Cidade objetivo) {
+	private boolean achou;
+
+	public profundidade(Cidade inicio, Cidade objetivo) {
 		this.inicio = inicio;
 		this.inicio.setVisitado(true);
 		this.objetivo = objetivo;
-		
+
 		fronteira = new Pilha(20);
 		fronteira.empilhar(inicio);
-		
+		achou = false;
 	}
-	
+
 	public void buscar() {
 		Cidade topo = fronteira.getTopo();
-		System.out.println("Topo: "+ topo.getName());
-	
-		for(Adjacente a:topo.getAdjacentes()) {
-			System.out.println("Verificando se já visitado: " + a.getCidade().getName());
-			if(!a.getCidade().isVisitado()) {
-				a.getCidade().setVisitado(true);
-				fronteira.empilhar(a.getCidade());
-				buscar();
+		System.out.println("Topo: " + topo.getName());
+
+		if (topo == objetivo) {
+			achou = true;
+		} else {
+
+			for (Adjacente a : topo.getAdjacentes()) {
+				if (!achou) {
+					System.out.println("Verificando se já visitado: " + a.getCidade().getName());
+					if (!a.getCidade().isVisitado()) {
+						a.getCidade().setVisitado(true);
+						fronteira.empilhar(a.getCidade());
+						buscar();
+					}
+				}
 			}
 		}
-		System.out.println("Desempilhous: "+fronteira.desempilhar().getName());
+		System.out.println("Desempilhous: " + fronteira.desempilhar().getName());
 	}
-	
-	//TESTE
+
+	// TESTE
 	public static void main(String args[]) {
 		Mapa mapa = new Mapa();
-		
+
 		profundidade p = new profundidade(mapa.getPortoUniao(), mapa.getCuritiba());
 		p.buscar();
 	}
